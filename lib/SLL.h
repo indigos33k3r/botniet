565dae0ac1bc4c51d75c64fc7da10ef70006e76b
Sivert V. Sæther <sivert@sivert.tech>
2018-06-24 23:31:20 +0200

    SLL

--- lib/SLL.h
+++ lib/SLL.h
@@ -12,11 +12,11 @@ class SLL {
 	node *head, *tail;
 	public:
 	SLL();
-	void createnode(struct *peer, bool active);
+	void createnode(struct sockaddr *peer, bool active);
 	const char *display();
-	void insert_start(struct *peer, bool active);
-	void insert_end(struct *peer, bool active);
-	void insert_position(int pos, struct *peer, bool active);
+	void insert_start(struct sockaddr *peer, bool active);
+	void insert_end(struct sockaddr *peer, bool active);
+	void insert_position(int pos, struct sockaddr *peer, bool active);
 	void delete_first();
 	void delete_last();
 	void delete_position(int pos);
